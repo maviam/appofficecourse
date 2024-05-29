34c09b9828d3c64dd483667fce668362a58fae06
Marcelo Amorim <marceloamorim.fa@gmail.com>
2024-05-29 22:47:48 +0100

    Create classes Class, Student and Grade

--- course/models.py
+++ course/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
 class TrainingUnit(models.Model):
@@ -11,3 +13,19 @@ class TrainingUnit(models.Model):
     
     def __str__(self):
         return f'{self.code} - { self.subject}'
+
+class Class(models.Model):
+    acronym = models.CharField(max_length=2)
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    birthday = models.DateField()
+    sex = models.CharField(max_length=6, choices=[('F','Female'),('M','Male')])
+    _class = models.ForeignKey(Class, on_delete=models.SET_NULL, null=True)
+    
+class Grade(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    training_unit = models.ForeignKey(TrainingUnit, on_delete=models.SET_NULL, null=True)
+    grade = models.FloatField()
+    released_date = models.DateTimeField(default=timezone.now)
