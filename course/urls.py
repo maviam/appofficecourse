949d85cbbdcba641ae106f5090f4f36f491643d6
Marcelo Amorim <marceloamorim.fa@gmail.com>
2024-05-31 18:43:04 +0100

    Data Class Page

--- course/urls.py
+++ course/urls.py
@@ -4,5 +4,6 @@ from course import views
 app_name = 'course'
 
 urlpatterns = [
-    path('', views.index, name='class_index')
+    path('', views.index, name='class_index'),
+    path('course/maindata/', views.main_data, name='main_data') # type: ignore
 ]
--- course/views/class_views.py
+++ course/views/class_views.py
@@ -1,15 +1,31 @@
 from django.shortcuts import render
-from course.models import Class
+from course.models import Class, TrainingUnit, Student
 
 # Create your views here.
 def index(request):
     classes = Class.objects.all()
     context = {
         'page_title': 'Office Course',
-        'classes': classes
+        'classes': classes,
+        'header': False
     }
     return render(
         request,
         'course/index.html',
+        context
+    )
+
+def main_data(request):
+    students = Student.objects.all()
+    training_units = TrainingUnit.objects.all()
+    context = {
+        'page_title': 'Class Students and Training Units',
+        'students': students,
+        'units': training_units,
+        'header': True
+    }
+    return render(
+        request,
+        'course/main_data.html',
         context
     )
